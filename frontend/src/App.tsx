b63fd9422bf736092dab6133d948c721a2dc0040
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-08-16 10:23:01 +0200

    frontend: format code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,24 +3,24 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1>Hello world</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        This is an app built with vite, react and typescript
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1>Hello world</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                This is an app built with vite, react and typescript
+            </p>
+        </>
+    )
 }
 
 export default App
