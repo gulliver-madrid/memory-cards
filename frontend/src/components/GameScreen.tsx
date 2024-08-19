e5a9a919a3a497e01712b77f977f381b37f559fd
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-08-19 18:25:33 +0200

    debugging

--- frontend/src/components/GameScreen.tsx
+++ frontend/src/components/GameScreen.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { SetNavState } from '../types'
 import ExitButton from './ExitButton'
 import './GameScreen.css'
@@ -26,62 +26,61 @@ const buildCardsData = () => {
     }
     return cardsData
 }
-const start = Date.now()
 
 const cardsData: ReadonlyArray<CardData> = buildCardsData()
 
-const displayElapsedTime = () => {
-    const millis = Date.now() - start
-    console.log(`\nseconds elapsed = ${Math.floor(millis / 1000)}`)
-}
-
 const GameScreen = ({ userName, setNavState }: Props) => {
+    const intervalIdRef = useRef(0)
     const [sequence, setSequence] = useState<CardData[]>([])
-    const [currentStep, setCurrentStep] = useState(0)
-    const [showingSequence, setShowingSequence] = useState(true)
-    displayElapsedTime()
-    console.log('Renderizando')
-    console.log({ currentStep })
+    const [currentStep, setCurrentStep] = useState(-1)
 
+    const showingSequence =
+        sequence.length && currentStep >= 0 && currentStep < sequence.length
+    const cardValue = showingSequence ? sequence[currentStep] : null
     useEffect(() => {
         //  Generate a random sequence of 3 cards
         const generatedSequence = Array.from(
             { length: 3 },
             () => cardsData[Math.floor(Math.random() * cardsData.length)]
         )
+        console.log(JSON.stringify(generatedSequence))
         setSequence(generatedSequence)
 
         // Display the cards sequentially
-        let step = 0
         console.log('definiendo el intervalo')
-        const interval = setInterval(() => {
-            displayElapsedTime()
-            console.log('En el intervalo')
-            console.log({ step })
-            setCurrentStep(step)
-            if (step >= generatedSequence.length) {
-                clearInterval(interval)
-                setShowingSequence(false)
-            }
-            step++
-        }, 1000)
+        console.log('current: ' + intervalIdRef.current)
+        intervalIdRef.current = setInterval(() => {
+            console.log(Date.now())
+            console.log(
+                `ejecutando el intervalo (previamente el id era ${intervalIdRef.current})`
+            )
 
-        return () => clearInterval(interval)
+            setCurrentStep((step) => step + 1)
+
+            console.log('finalizando ejecucion de intervalo')
+        }, 2000)
+        console.log('creado un intervalo con id ' + intervalIdRef.current)
+
+        return () => clearInterval(intervalIdRef.current)
     }, [])
+    if (currentStep >= sequence.length) {
+        clearInterval(intervalIdRef.current)
+    }
 
     return (
         <div>
             <p>User: {userName}</p>
             <p>The Game has started!</p>
             <div className="game-screen">
-                {showingSequence && sequence.length ? (
+                {cardValue ? (
                     <div className="card-display">
                         <p>currentStep: {currentStep}</p>
-                        {sequence[currentStep].shape}{' '}
-                        {sequence[currentStep].color}
+                        {cardValue.shape} {cardValue.color}
                     </div>
-                ) : (
+                ) : currentStep > 0 ? (
                     <p>Cards displayed</p>
+                ) : (
+                    <p>Waiting...</p>
                 )}
             </div>
             <ExitButton setNavState={setNavState} />
