69af0be6aa19eb2415220375298d1d07c9158042
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-08-21 14:59:46 +0200

    jest integration

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,3 +1,13 @@
 export default {
+    preset: 'ts-jest',
+    testEnvironment: 'jest-environment-jsdom',
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+        // process `*.tsx` files with `ts-jest`
+    },
     rootDir: 'src',
+    moduleNameMapper: {
+        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
+        '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    },
 }
