25ed52eb51653f77afe479dedd1e5ec224e2b866
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-08-22 12:46:44 +0200

    frontend: test: extract dataSequence

---
+++ frontend/src/__tests__/examples.ts
@@ -0,0 +1,9 @@
+import { Color, Shape } from '../types'
+
+const sequenceData: [Shape, Color][] = [
+    ['square', 'green'],
+    ['triangle', 'red'],
+    ['circle', 'blue'],
+]
+
+export { sequenceData }
--- frontend/src/__tests__/model.test.ts
+++ frontend/src/__tests__/model.test.ts
@@ -1,21 +1,21 @@
 import { createCard, getResult } from '../model'
-import { Color, Shape } from '../types'
-
-const data1: [Shape, Color][] = [
-    ['square', 'green'],
-    ['triangle', 'red'],
-    ['circle', 'blue'],
-]
+import { sequenceData } from './examples'
 
 test('getResult=>true', () => {
-    const sequence1 = data1.map(([shape, color]) => createCard(shape, color))
-    const sequence2 = data1.map(([shape, color]) => createCard(shape, color))
+    const sequence1 = sequenceData.map(([shape, color]) =>
+        createCard(shape, color)
+    )
+    const sequence2 = sequenceData.map(([shape, color]) =>
+        createCard(shape, color)
+    )
     expect(getResult(sequence1, sequence2)).toBe(true)
 })
 
 test('getResult=>false', () => {
-    const sequence1 = data1.map(([shape, color]) => createCard(shape, color))
-    const data2 = [...data1]
+    const sequence1 = sequenceData.map(([shape, color]) =>
+        createCard(shape, color)
+    )
+    const data2 = [...sequenceData]
     data2[2] = ['circle', 'red']
     const sequence2 = data2.map(([shape, color]) => createCard(shape, color))
     expect(getResult(sequence1, sequence2)).toBe(false)
