4584592606e8da31c979289e91c3ece1b459ed5e
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-08-22 15:50:22 +0200

    frontend: add dropdown to select a demo

--- frontend/src/demos/Demo.tsx
+++ frontend/src/demos/Demo.tsx
@@ -1,7 +1,49 @@
+import { useState } from 'react'
 import './Demo.css'
+import DemoCard from './DemoCard'
+
+type SelectOption = 'demo-home' | 'demo-card'
 
 const Demo = () => {
-    return <div className="demo">Demos</div>
+    const [demoName, setDemoName] = useState<SelectOption>('demo-home')
+    const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) =>
+        setDemoName(e.target.value as SelectOption)
+    return (
+        <>
+            <select
+                id="demo-select"
+                value={demoName}
+                onChange={handleSelectChange}
+            >
+                <option value="demo-home" selected>
+                    Demos Home
+                </option>
+                <option value="demo-card">DemoCard</option>
+            </select>
+            <div className="demo">
+                {(() => {
+                    switch (demoName) {
+                        case 'demo-home':
+                            return (
+                                <div>
+                                    <h2>Demos</h2>
+                                    <p>
+                                        From here it's possible to explore the
+                                        demos
+                                        <br />
+                                        (just use the dropdown at the top)
+                                    </p>
+                                </div>
+                            )
+                        case 'demo-card':
+                            return <DemoCard />
+                        default:
+                            return <p>Unknown option</p>
+                    }
+                })()}
+            </div>
+        </>
+    )
 }
 
 export default Demo
