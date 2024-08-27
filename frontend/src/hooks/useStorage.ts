4ff72ceaabb35a5b9a32bd6a1c0f613cb0effcf1
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-08-27 13:05:07 +0200

    frontend: refactor useStorage using inner functions

--- frontend/src/hooks/useStorage.ts
+++ frontend/src/hooks/useStorage.ts
@@ -1,19 +1,22 @@
 const APP_KEY = 'memory-cards'
 
 const useStorage = () => {
-    const getStorageDict = () => {
+    const _getStorageDict = () => {
         const jsonData = window.localStorage.getItem(APP_KEY) || '{}'
         return JSON.parse(jsonData)
     }
+    const _saveStorageDict = (data: unknown) => {
+        const jsonData = JSON.stringify(data)
+        window.localStorage.setItem(APP_KEY, jsonData)
+    }
     const read = (key: string): unknown => {
-        const data = getStorageDict()
+        const data = _getStorageDict()
         return data[key]
     }
     const write = (key: string, value: unknown): void => {
-        const data = getStorageDict()
+        const data = _getStorageDict()
         data[key] = value
-        const jsonData = JSON.stringify(data)
-        window.localStorage.setItem(APP_KEY, jsonData)
+        _saveStorageDict(data)
     }
     return { read, write }
 }
