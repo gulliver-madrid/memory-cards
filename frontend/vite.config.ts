1e6c08b9ead385e94ddcc0e250411a27ef57eea9
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-08-28 17:00:11 +0200

    frontend: add config for CSS modules

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    css: {
+        modules: {
+            generateScopedName: '[name]_[local]__[hash:base64:5]',
+            localsConvention: 'camelCaseOnly',
+        },
+    },
 })
