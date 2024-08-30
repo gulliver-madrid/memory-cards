2efdcd304351f3162706060c664899a1becf820b
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-08-30 15:43:05 +0200

    frontend: fix jest-CSS modules compatibility....
    ...for GameWidget test.

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
 import App from '../App'
+import '../types/index.d.ts'
 
 test('Renders the main page', () => {
     render(<App />)
