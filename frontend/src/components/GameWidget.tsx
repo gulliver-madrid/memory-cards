520c260185f96a871cedd2a1158065a06d663d1f
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-09-01 07:35:53 +0200

    frontend: refactor: move GameWidgetContent...
    ... to its own file.

--- frontend/src/components/GameWidget.tsx
+++ frontend/src/components/GameWidget.tsx
@@ -1,13 +1,10 @@
 import { useRef } from 'react'
-import { useTranslation } from 'react-i18next'
-import useGame, { GameView } from '../hooks/useGame'
+import useGame from '../hooks/useGame'
 import { reprCardData } from '../model'
 import { CardData, Game } from '../types'
 import { repr } from '../utils'
-import Card from './Card'
-import CardSequence from './CardSequence'
-import CardsToClick from './CardsToClick'
 import styles from './GameWidget.module.css'
+import GameWidgetContent from './GameWidgetContent'
 
 interface Props {
     gameIndex: number
@@ -64,83 +61,6 @@ const GameWidget = ({
                 numberOfCardsToRemember={numberOfCardsToRemember}
             />
         </div>
-    )
-}
-
-interface GameWidgetContentProps extends GameView {
-    numberOfCardsToRemember: number
-}
-
-const GameWidgetContent = ({
-    status,
-    win,
-    cardValue,
-    sequenceToRemember,
-    userSequence,
-    addCard,
-    numberOfCardsToRemember,
-}: GameWidgetContentProps) => {
-    const { t } = useTranslation()
-
-    switch (status) {
-        case 'initial':
-            return <p>{t('Game starting', { n: numberOfCardsToRemember })}</p>
-        case 'showing-cards':
-            return (
-                cardValue && (
-                    <div className={styles.cardDisplay}>
-                        <Card shape={cardValue.shape} color={cardValue.color} />
-                    </div>
-                )
-            )
-        case 'pause-before-answering':
-            return <p>{t('Cards displayed')}</p>
-        case 'answering':
-            return (
-                <>
-                    <div className={styles.container}>
-                        <CardsToClick addCard={addCard} />
-                    </div>
-                    <SmallCardSequenceWithTitle
-                        title={t('Your sequence')}
-                        sequence={userSequence}
-                    />
-                </>
-            )
-        case 'showing-results':
-            return (
-                <>
-                    <h2>{win === true ? t('win') : t('lost')}</h2>
-                    <SmallCardSequenceWithTitle
-                        title={t('Your sequence')}
-                        sequence={userSequence}
-                    />
-                    <SmallCardSequenceWithTitle
-                        title={t('Actual sequence')}
-                        sequence={sequenceToRemember}
-                    />
-                </>
-            )
-        default:
-            console.error('Unknown status: ' + status)
-            return
-    }
-}
-
-const SmallCardSequenceWithTitle = ({
-    title,
-    sequence,
-}: {
-    title: string
-    sequence: ReadonlyArray<CardData>
-}) => {
-    return (
-        <>
-            <p>{title}</p>
-            <div className={styles.cardSequence}>
-                <CardSequence sequence={sequence} scale={0.4} />
-            </div>
-        </>
     )
 }
 
---
+++ frontend/src/components/GameWidgetContent.tsx
@@ -0,0 +1,85 @@
+import { useTranslation } from 'react-i18next'
+import { GameView } from '../hooks/useGame'
+import { CardData } from '../types'
+import Card from './Card'
+import CardSequence from './CardSequence'
+import CardsToClick from './CardsToClick'
+import styles from './GameWidgetContent.module.css'
+
+interface Props extends GameView {
+    numberOfCardsToRemember: number
+}
+
+const GameWidgetContent = ({
+    status,
+    win,
+    cardValue,
+    sequenceToRemember,
+    userSequence,
+    addCard,
+    numberOfCardsToRemember,
+}: Props) => {
+    const { t } = useTranslation()
+
+    switch (status) {
+        case 'initial':
+            return <p>{t('Game starting', { n: numberOfCardsToRemember })}</p>
+        case 'showing-cards':
+            return (
+                cardValue && (
+                    <div className={styles.cardDisplay}>
+                        <Card shape={cardValue.shape} color={cardValue.color} />
+                    </div>
+                )
+            )
+        case 'pause-before-answering':
+            return <p>{t('Cards displayed')}</p>
+        case 'answering':
+            return (
+                <>
+                    <div className={styles.container}>
+                        <CardsToClick addCard={addCard} />
+                    </div>
+                    <SmallCardSequenceWithTitle
+                        title={t('Your sequence')}
+                        sequence={userSequence}
+                    />
+                </>
+            )
+        case 'showing-results':
+            return (
+                <>
+                    <h2>{win === true ? t('win') : t('lost')}</h2>
+                    <SmallCardSequenceWithTitle
+                        title={t('Your sequence')}
+                        sequence={userSequence}
+                    />
+                    <SmallCardSequenceWithTitle
+                        title={t('Actual sequence')}
+                        sequence={sequenceToRemember}
+                    />
+                </>
+            )
+        default:
+            console.error('Unknown status: ' + status)
+            return
+    }
+}
+
+const SmallCardSequenceWithTitle = ({
+    title,
+    sequence,
+}: {
+    title: string
+    sequence: ReadonlyArray<CardData>
+}) => {
+    return (
+        <>
+            <p>{title}</p>
+            <div className={styles.cardSequence}>
+                <CardSequence sequence={sequence} scale={0.4} />
+            </div>
+        </>
+    )
+}
+export default GameWidgetContent
