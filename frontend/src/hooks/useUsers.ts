949718494026859b83d65ff244f99306056af7ac
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-09-01 13:25:04 +0200

    frontend refactor: extract useUsers inner function outside

--- frontend/src/hooks/useUsers.ts
+++ frontend/src/hooks/useUsers.ts
@@ -1,31 +1,17 @@
 import { useEffect, useState } from 'react'
-import { useTranslation } from 'react-i18next'
 import backendService from '../backendService'
+import i18n from '../i18n'
 import { Game, User } from '../types'
 
 const ERR_USERNAME_NOT_VALID = 'ERR_USERNAME_NOT_VALID'
 const ERR_USER_ALREADY_EXISTS = 'ERR_USER_ALREADY_EXISTS'
 
 const useUsers = () => {
-    const { t } = useTranslation()
     const [usersMap, setUsersMap] = useState<null | ReadonlyMap<string, User>>(
         null
     )
-
     const [createUserErr, setCreateUserErr] = useState<string | null>(null)
-    const getMessageFromErrCode = (
-        errCode: string | null,
-        newName: string
-    ): string => {
-        switch (errCode) {
-            case ERR_USERNAME_NOT_VALID:
-                return t('user name not valid', { name: newName })
-            case ERR_USER_ALREADY_EXISTS:
-                return t('user already exists', { user: newName })
-            default:
-                return t('unknown error')
-        }
-    }
+
     useEffect(() => {
         const loadUsersFromBackend = async () => {
             const newUsersMap = await backendService.getUsers()
@@ -100,6 +86,20 @@ const useUsers = () => {
         commitUser,
         createUserErr,
         addGame,
+    }
+}
+
+const getMessageFromErrCode = (
+    errCode: string | null,
+    newName: string
+): string => {
+    switch (errCode) {
+        case ERR_USERNAME_NOT_VALID:
+            return i18n.t('user name not valid', { name: newName })
+        case ERR_USER_ALREADY_EXISTS:
+            return i18n.t('user already exists', { user: newName })
+        default:
+            return i18n.t('unknown error')
     }
 }
 
